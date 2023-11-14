97ac6095460f5c1f3c339e8c5668e56ddf6eb5c3
sycz00 <fabian.schmalstieg@gmx.de>
2023-11-14 13:39:52 +0100

    added comments

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 NITI_PATH="/media/fa/Shared_Files/Fraunhofer/niti"
 GPU=0
+#build argument -t name-tag and the point stands for everything
 docker build -t wangmaolin/niti:0.1 .
 
+#--shm-size			Size of /dev/shm
+#--runtime			Runtime to use for this container
+#--env	-e		Set environment variables
+#-v Bind mount a volume Easy way to volume mount on docker run command to exchange files between container and host
+# -v /path/from/your/host:/path/inside/the/container
+#   <-------host------->:<--------container------->
+#--interactive	-i		Keep STDIN open even if not attached
+#--tty	-t		Allocate a pseudo-TTY
 docker run --shm-size=2g --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=$GPU -v $NITI_PATH:/niti -it wangmaolin/niti:0.1
--- train_vgg_cifar10.sh
+++ train_vgg_cifar10.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-# python main.py --depth 7 --save int-vgg-small-7
+python main.py --depth 7 --save int-vgg-small-7
 # python main.py --depth 8 --save int-vgg-small-8
 # python main.py --depth 9 --save int-vgg-small-9
-python main.py --depth 7 --save test 
+# python main.py --depth 7 --save test 
